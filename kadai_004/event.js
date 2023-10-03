c325f1b3e8d4c1d21709ebdec2e462c10f1e6650
nozawamitsuki <kaminagara13@gmail.com>
2023-10-03 20:58:31 +0900

    kadai_004の提出

---
+++ kadai_004/event.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(window).on({
+        'scroll':()=>{
+            console.log('scrollイベントが発生しました');
+        }
+    });
+});
+$(window).on({
+    'load':()=>{
+        console.log('loadイベントが発生しました');
+    },
+});
